4bb1905e2372b5551ed1f3d2df3dac5e5a9385c8
jintao-wang <wangjintaoapp@gmail.com>
2021-03-11 17:50:25 +0800

    调整样式位置

--- projects/solutions/sales_office/components/sand_table/sandTable.js
+++ projects/solutions/sales_office/components/sand_table/sandTable.js
@@ -7,134 +7,6 @@ import { BuildList } from '../../config';
 import ZTop from '../../../../../components/z_top/1.0';
 import GlobalClose from '../../../../../components/global-close/2.0';
 
-const ContainerSC = styled('div', ['aniTrigger'])`
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  opacity: ${(props) => (props.aniTrigger ? 1 : 0)};
-  transition: opacity .2s;
-  
-`;
-
-const SandSC = styled('div', ['sandImgUrl'])`
-  width: 100%;
-  height: 100%;
-  background: rgb(0,0,0) url('https://vrhouse-web.oss-cn-shanghai.aliyuncs.com/next-solution/sales-office/static/building.jpg') no-repeat center / cover;
-`;
-
-const PointSC = styled('div', ['left', 'bottom', 'rotate', 'selected', 'active'])`
-  position: absolute;
-  left: ${(props) => props.left || 0}px;
-  bottom: ${(props) => props.bottom || 0}px;
-  transform: rotate(${(props) => props.rotate || 0}deg);
-  border: ${(props) => (props.selected ? '1px solid #000' : '1px solid rgba(0,0,0,0)')};
-  transition: border .2s;
-  cursor: pointer;
-  
-  img {
-    width: 26px;
-  }
-  
-  .info {
-    position: absolute;
-    color: ${(props) => (props.active ? 'rgba(51,51,51,1)' : 'orange')};
-    left: 0;
-    right: 0;
-    top: 4px;
-    bottom: 0;
-    margin: auto;
-    text-align: center;
-    font-size: 14px;
-  }
-`;
-
-const RecommendViewSC = styled('div')`
-  width: 100%;
-  height: 280px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 10;
-  background: white;
-`;
-
-const RecommendItemSC = styled('div', ['url'])`
-  width: 100vw;
-  height: 250px;
-  margin: 0;
-  padding-bottom: 30px;
-  background: white;
-  
-  .img {
-    width: 100%;
-    height: 100%;
-    background: white url(${(props) => props.url}) no-repeat center / contain;
-  }
-`;
-
-const BigIconSC = styled('div')`
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  top: 20px;
-  right: 15px;
-  z-index: 1;
-  color: #666666;
-  font-size: 20px;
-  text-align: right;
-`;
-
-const RecommendBigContainerSC = styled('div')`
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  position: fixed;
-  background: rgba(0,0,0,.5);
-  backdrop-filter: Blur(5px);
-  z-index: 10000;
-  pointer-events: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const TitleSC = styled('div')`
-    letter-spacing: 2px;
-    padding: 6px 15px;
-    background: rgba(0,0,0,.7);
-    border-radius: 18px;
-    font-size: 13px;
-    font-family: PingFangSC-Medium,PingFang SC;
-    font-weight: 500;
-    color: rgba(255,255,255,1);
-    display: flex;
-    position: absolute;
-    top: 35px;
-`;
-
-const ViewBigSC = styled('div')`
-  width: 100vw;
-  height: 480px;
-`;
-
-const ViewBigItemSC = styled('div', ['url'])`
-  width: calc(100vw - 60px);
-  height: 480px;
-  margin: 0 8px;
-  padding-bottom: 30px;
-  background: white;
-  box-sizing: border-box;
-  
-  .img {
-    width: 100%;
-    height: 100%;
-    background: white url(${(props) => props.url}) no-repeat center / contain;
-  }
-`;
-
 const SandTable = ({
   visible,
   sandImgUrl,
@@ -321,3 +193,131 @@ SandTable.defaultProps = {
 };
 
 export default SandTable;
+
+const ContainerSC = styled('div', ['aniTrigger'])`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  opacity: ${(props) => (props.aniTrigger ? 1 : 0)};
+  transition: opacity .2s;
+  
+`;
+
+const SandSC = styled('div', ['sandImgUrl'])`
+  width: 100%;
+  height: 100%;
+  background: rgb(0,0,0) url('https://vrhouse-web.oss-cn-shanghai.aliyuncs.com/next-solution/sales-office/static/building.jpg') no-repeat center / cover;
+`;
+
+const PointSC = styled('div', ['left', 'bottom', 'rotate', 'selected', 'active'])`
+  position: absolute;
+  left: ${(props) => props.left || 0}px;
+  bottom: ${(props) => props.bottom || 0}px;
+  transform: rotate(${(props) => props.rotate || 0}deg);
+  border: ${(props) => (props.selected ? '1px solid #000' : '1px solid rgba(0,0,0,0)')};
+  transition: border .2s;
+  cursor: pointer;
+  
+  img {
+    width: 26px;
+  }
+  
+  .info {
+    position: absolute;
+    color: ${(props) => (props.active ? 'rgba(51,51,51,1)' : 'orange')};
+    left: 0;
+    right: 0;
+    top: 4px;
+    bottom: 0;
+    margin: auto;
+    text-align: center;
+    font-size: 14px;
+  }
+`;
+
+const RecommendViewSC = styled('div')`
+  width: 100%;
+  height: 280px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  background: white;
+`;
+
+const RecommendItemSC = styled('div', ['url'])`
+  width: 100vw;
+  height: 250px;
+  margin: 0;
+  padding-bottom: 30px;
+  background: white;
+  
+  .img {
+    width: 100%;
+    height: 100%;
+    background: white url(${(props) => props.url}) no-repeat center / contain;
+  }
+`;
+
+const BigIconSC = styled('div')`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  top: 20px;
+  right: 15px;
+  z-index: 1;
+  color: #666666;
+  font-size: 20px;
+  text-align: right;
+`;
+
+const RecommendBigContainerSC = styled('div')`
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  position: fixed;
+  background: rgba(0,0,0,.5);
+  backdrop-filter: Blur(5px);
+  z-index: 10000;
+  pointer-events: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleSC = styled('div')`
+    letter-spacing: 2px;
+    padding: 6px 15px;
+    background: rgba(0,0,0,.7);
+    border-radius: 18px;
+    font-size: 13px;
+    font-family: PingFangSC-Medium,PingFang SC;
+    font-weight: 500;
+    color: rgba(255,255,255,1);
+    display: flex;
+    position: absolute;
+    top: 35px;
+`;
+
+const ViewBigSC = styled('div')`
+  width: 100vw;
+  height: 480px;
+`;
+
+const ViewBigItemSC = styled('div', ['url'])`
+  width: calc(100vw - 60px);
+  height: 480px;
+  margin: 0 8px;
+  padding-bottom: 30px;
+  background: white;
+  box-sizing: border-box;
+  
+  .img {
+    width: 100%;
+    height: 100%;
+    background: white url(${(props) => props.url}) no-repeat center / contain;
+  }
+`;
